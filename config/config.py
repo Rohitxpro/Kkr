d3890cbfa827921a890bee1e9f76e492f8d6a726
Rohitxpro <140487047+Rohitxpro@users.noreply.github.com>
2023-07-25 15:37:10 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -45,9 +45,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Infamous-Hydra/Miko-Music",
+    "https://github.com/rohitxpro/kkr",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
